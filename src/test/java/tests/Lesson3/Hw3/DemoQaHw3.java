513bb48ef772290d945c1590c41c68e61782ebbe
YuSer <ruslan.skvortsov@iw-group.pro>
2025-12-02 20:16:31 +0400

    Корректировки с датой

--- src/test/java/tests/Lesson3/Hw3/DemoQaHw3.java
+++ src/test/java/tests/Lesson3/Hw3/DemoQaHw3.java
@@ -21,7 +21,10 @@ public void demoQaHw3() {
     $(By.xpath("//*[@id='userEmail']")).sendKeys("Vasya@rt.ru");
     $(By.xpath("//label[@for='gender-radio-1']")).click();
     $(By.xpath("//*[@id='userNumber']")).sendKeys("1234567899");
-    $(By.xpath("//*[@id='dateOfBirthInput']")).pressEnter();
+    $(By.xpath("//*[@id='dateOfBirthInput']")).click();
+    $(By.xpath("//*[@class='react-datepicker__month-select']")).selectOption("June");
+    $(By.xpath("//*[@class='react-datepicker__year-select']")).selectOption("2000");
+    $(By.xpath("//*[@node()='Choose Friday, June 9th, 2000']")).click();
     $(By.xpath("//*[@id='subjectsInput']")).sendKeys("Computer Science");
     $(By.xpath("//*[@id='subjectsInput']")).pressEnter();
     $(By.xpath("//label[@for='hobbies-checkbox-1']")).click();
@@ -40,7 +43,7 @@ public void demoQaHw3() {
     $(By.xpath("//td[text()='Student Email']/following-sibling::td")).shouldHave(text("Vasya@rt.ru"));
     $(By.xpath("//td[text()='Gender']/following-sibling::td")).shouldHave(text("Male"));
     $(By.xpath("//td[text()='Mobile']/following-sibling::td")).shouldHave(text("1234567899"));
-    $(By.xpath("//td[text()='Date of Birth']/following-sibling::td")).shouldHave(text("02 December,2025"));
+    $(By.xpath("//td[text()='Date of Birth']/following-sibling::td")).shouldHave(text("09 June,2000"));
     $(By.xpath("//td[text()='Subjects']/following-sibling::td")).shouldHave(text("Computer Science"));
     $(By.xpath("//td[text()='Hobbies']/following-sibling::td")).shouldHave(text("Sports"));
     $(By.xpath("//td[text()='Picture']/following-sibling::td")).shouldHave(text("ToolSqa.jpg"));
